2c9645c3eda271e2ab4140182323ebebc349c3d2
MzhdaSangawi <94749754+MzhdaSangawi@users.noreply.github.com>
2021-12-07 15:28:00 +0300

    coding in notes edit

--- lib/screens/notes_edit.dart
+++ lib/screens/notes_edit.dart
@@ -106,3 +106,27 @@ class NoteTitleEntry extends StatelessWidget {
     );
   }
 }
+
+class NoteEntry extends StatelessWidget {
+  final _textFieldController;
+
+  NoteEntry(this._textFieldController);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: TextField(
+        controller: _textFieldController,
+        maxLines: null,
+        textCapitalization: TextCapitalization.sentences,
+        decoration: null,
+        style: TextStyle(
+          fontSize: 19,
+          height: 1.5,
+        ),
+      ),
+    );
+  }
+}
