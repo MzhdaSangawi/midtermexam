b425db498ef1bd39abc121834de0335e716c9c2c
MzhdaSangawi <94749754+MzhdaSangawi@users.noreply.github.com>
2021-12-07 15:26:04 +0300

    coding in  nots edit is countiung

--- lib/screens/notes_edit.dart
+++ lib/screens/notes_edit.dart
@@ -68,3 +68,41 @@ class _NotesEdit extends State<NotesEdit> {
     );
   }
 }
+
+class NoteTitleEntry extends StatelessWidget {
+  final _textFieldController;
+
+  NoteTitleEntry(this._textFieldController);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _textFieldController,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        errorBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+        contentPadding: EdgeInsets.all(0),
+        counter: null,
+        counterText: "",
+        hintText: 'Title',
+        hintStyle: TextStyle(
+          fontSize: 21,
+          fontWeight: FontWeight.bold,
+          height: 1.5,
+        ),
+      ),
+      maxLength: 31,
+      maxLines: 1,
+      style: TextStyle(
+        fontSize: 21,
+        fontWeight: FontWeight.bold,
+        height: 1.5,
+        color: Color(c1),
+      ),
+      textCapitalization: TextCapitalization.words,
+    );
+  }
+}
